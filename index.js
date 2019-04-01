f4dbfafe69465d782937847a8e2a8ab5a8ad479f
Melissa Real <melissa.real@hotmail.com>
2019-04-01 06:33:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function Scooter(year, color, model){
+  this.year;
+  this.color;
+  this.model;
+}
+
+function Driver(name, age, experience){
+  this.name;
+  this.age;
+  this.experience;
+}
+function PickupLocation(address, city){
+  this.address;
+  this.city;
+}
